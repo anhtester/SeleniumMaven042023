87c074662ecba69d4c5a660ec71e9fe077741dcf
Anh Tester <thaian.it15@gmail.com>
2023-08-03 02:04:19 +0700

    Bài 9 TestNG Framework

---
+++ src/test/java/com/anhtester/Bai9_TestNGFramework/LoginTest2.java
@@ -0,0 +1,44 @@
+package com.anhtester.Bai9_TestNGFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginTest2 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void createDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+
+    @Test(priority = 1)
+    public void testAnhTesterBlog3() throws InterruptedException {
+        driver.get("https://anhtester.com");
+        driver.findElement(By.xpath("//a[normalize-space()='blog']")).click();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 3)
+    public void testAnhTesterBlog4() throws InterruptedException {
+        driver.get("https://anhtester.com");
+        driver.findElement(By.xpath("//a[normalize-space()='blog']")).click();
+        Thread.sleep(2000);
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/com/anhtester/PackageDemo/LoginTest3.java
@@ -0,0 +1,43 @@
+package com.anhtester.PackageDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginTest3 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void createDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+
+    @Test(priority = 1)
+    public void testAnhTesterBlog3() throws InterruptedException {
+        driver.get("https://anhtester.com");
+        driver.findElement(By.xpath("//a[normalize-space()='blog']")).click();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 3)
+    public void testAnhTesterBlog4() throws InterruptedException {
+        driver.get("https://anhtester.com");
+        driver.findElement(By.xpath("//a[normalize-space()='blog']")).click();
+        Thread.sleep(2000);
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        driver.quit();
+    }
+
+}
